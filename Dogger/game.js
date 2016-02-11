935d87e6d664eeeb9d7b24a75d86b4b4263b1ce7
nickpatrick0922 <nickforetich@protonmail.ch>
2016-02-11 13:13:04 -0500

    s

--- Dogger/game.js
+++ Dogger/game.js
@@ -279,7 +279,16 @@ function update()
 						}
 					}
 
-					
+					if(CHAR_START_Y+CHAR_HEIGHT==STAGE_HEIGHT && CHAR_START_X<85)
+					{
+						window.alert("You win!");
+						CHAR_START_X = 520;
+						CHAR_START_Y = 10;
+						else {
+
+						}
+
+					}
 
 
 
